1cbb86a0756bcd08e2fc9842c0fa3a9441744ccd
unknown <Elamathi Elangovan@W1033WLPF2.aus.amer.dell.com>
2022-10-02 19:41:43 -0600

    Python-challange consist both PyBank and PyPoll assignment

---
+++ PyBank/main.py
@@ -0,0 +1,76 @@
+# Python script to analyze the financial records.
+#To calculate the total number of months included in the dataset.
+#The net total amount of "Profit/Losses" over the entire period and the average.
+#To calculate the greatest increase and decrease in profits over the entire period.
+
+#Import the os  and csv module to read the .csv file in the resources.
+from datetime import date
+import os
+import csv
+from statistics import mean
+
+#defining variable
+date = []
+total_list = []
+count_months =0
+total_sum=0
+change=0
+change_list=[]
+total_change=0
+ave_change=0
+max_change=0
+min_change=0
+min_change_date=0
+max_change_date=0
+pmonth=0
+
+#file path
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#with function to read the csv file
+with open(csvpath, 'r') as csvfile:
+
+    #csv reader specifies the variable and delimiter
+    csvreader=csv.reader(csvfile, delimiter=',')
+
+    #To remove the header file
+    header=next(csvreader)
+
+    #for loop to read the .csv row by row
+    for row in csvreader:
+        date.append(row[0])
+        total_list.append(row[1])
+        count_months+=1
+        total_sum+=int(row[1])
+        #If statement to determine the change in "Profit/Losses"
+        if pmonth!=0:
+          change = int(row[1]) - int(pmonth)
+          change_list.append(change)
+          pmonth=row[1]
+        else:
+          pmonth=row[1]  
+        #If statement to determine the greatest increase and decrease in profits  
+        if change >max_change:
+          max_change=change
+          max_change_date=row[0]
+        elif change<min_change:
+          min_change=change
+          min_change_date=row[0]  
+            
+    #To calculate the average of the changes
+    ave_change=round(mean(change_list),2)
+    
+    
+#defining path for output.textfile in Analysis folder
+output=os.path.join('Analysis', 'output.txt')     
+#to write the putput in the output.textfile         
+with open(output, 'w') as textfile:
+     textfile.write('Financial Analysis\n'
+    '----------------------------\n'
+    f'Total Months: {count_months}\n'
+    f'Total: ${total_sum}\n'
+    f'Average Change: ${ave_change}\n'
+    f'Greatest Increase in Profits: {max_change_date}(${max_change})\n'
+    f'Greatest Decrease in Profits: {min_change_date}(${min_change})')
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,75 @@
+#PyPoll -Homework assignment
+#Python script - To read the data from .csv file
+#To calculate the total number of votes cast.
+#A complete list of candidates who received votes.
+#The total number of votes and percentage of their votes.
+#The winner based on the popular vote
+import os
+import csv
+
+#file path
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+#defining variable
+ballot_id = []
+count = 0
+candidate_list=[]
+candidate_votes={}
+votes=0
+winner=0
+winner_candidate=""
+
+#with function to read the csv file
+with open(csvpath, 'r') as csvfile:
+
+    #csv reader specifies the variable and delimiter
+    csvreader=csv.reader(csvfile, delimiter=',')
+
+    #To remove the header file
+    header=next(csvreader)
+    #for loop to count the total id
+    for row in csvreader:
+       ballot_id.append(row[0])
+       candidate_name = row[2]
+       count += 1
+       # If the candidate does not match any existing candidate add it the
+       # the candidate list
+       if candidate_name not in candidate_list:
+           # Add the candidate name to the candidate list.
+           candidate_list.append(candidate_name)
+           #Tracking the candidate's voter count.
+           candidate_votes[candidate_name] = 0
+           #Add a vote to that candidate's count
+           candidate_votes[candidate_name] += 1
+       else:
+            # Add a vote to that candidate's count
+            candidate_votes[candidate_name] += 1
+            
+#defining path for output.textfile in Analysis folder
+output=os.path.join('Analysis', 'output.txt')   
+
+#to write the output in the output.textfile 
+with open(output, 'w') as textfile:   
+    textfile.write('Election Results\n'
+                '----------------------------------------\n'
+                f'Total Vote:{count}\n'
+                '----------------------------------------\n')              
+    #for loop to calculate the percent
+    for candidate in candidate_votes:
+        votes=candidate_votes[candidate]
+        percent=float(votes/count)*100
+        #if loop to determine the winner candidate with highest vote count
+        if (votes>winner):
+            winner=votes
+            winner_candidate=candidate
+        textfile.write(f"{candidate}: {percent:.3f}% ({votes:})\n")
+                    
+
+    textfile.write(f'----------------------------------------\n'
+                   f'winner : {winner_candidate}\n'
+                   f'----------------------------------------\n')            
+
+  
+        
+
+     
